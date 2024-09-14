1fb562bea5105181df05a589660e267d7e212797
Fabo <faboale020323@gmail.com>
2024-09-14 18:43:44 -0400

    Subiendo nuevo proyecto

---
+++ src/main/java/com/part_of_fa/usuarios_documentos/UsuariosDocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.part_of_fa.usuarios_documentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosDocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosDocumentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/part_of_fa/usuarios_documentos/UsuariosDocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.part_of_fa.usuarios_documentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosDocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
